5f7540a0260292833cdf458258acd06b3d844660
Kim Burmester <Kim.Burmester@mailbox.org>
2025-05-26 18:22:39 +0200

    feat: add basket function to open in mobile

---
+++ script/basket.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const toggle = document.getElementById("basket-toggle");
+  const basket = document.querySelector(".basket");
+
+  toggle.addEventListener("click", function() {
+    basket.classList.toggle("open");
+  });
+});
