7f1c2bdee32954e396273f8113d0896b9e320688
Kim Burmester <Kim.Burmester@mailbox.org>
2025-05-28 21:24:19 +0200

    updated

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-import { dishes } from "/script/db";
+let dishes = [
+  {
+    name: "Spaghetti Bolognese",
+    description: "Hausgemachte Spaghetti mit traditioneller Rindfleisch-Bolognese",
+    price: 11.50
+  },
+  {
+    name: "Margherita Pizza",
+    description: "Knuspriger Pizzaboden mit Tomatensauce, Mozzarella und frischem Basilikum",
+    price: 9.90
+  },
+  {
+    name: "Caesar Salad",
+    description: "Römersalat mit Hähnchenstreifen, Croutons und Caesar-Dressing",
+    price: 10.20
+  },
+  {
+    name: "Kürbisrisotto",
+    description: "Cremiges Risotto mit Hokkaidokürbis und Parmesan",
+    price: 12.30
+  },
+  {
+    name: "Lachsfilet mit Gemüse",
+    description: "Gebratenes Lachsfilet auf einem Bett aus mediterranem Gemüse",
+    price: 14.80
+  }
+];
 
+function dishesInit() {
+    renderDishes();
+}
+
+function renderDishes() {
+
+}
