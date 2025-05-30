394e9d51841b4549605c24b66944786bbaa774f4
Kim Burmester <Kim.Burmester@mailbox.org>
2025-05-30 13:23:31 +0200

    fix: basket changed for mobile style.

--- script/basket.js
+++ script/basket.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* document.addEventListener("DOMContentLoaded", function() {
   const toggle = document.getElementById("basket-toggle");
   const basket = document.querySelector(".basket");
 
   toggle.addEventListener("click", function() {
     basket.classList.toggle("open");
   });
-});
+}); */
 
 
 /*//NOTE: Move To Basket Logic */
-
+document.getElementById('basket-toggle')
+  .addEventListener('click', () => {
+    document.querySelector('.basket_wrapper')
+      .classList.toggle('open');
+  });
